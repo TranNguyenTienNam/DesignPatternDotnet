ae50ab300dce80d1ca85d3b0e5b67616085def3b
Tiến Nam <19521871@gm.uit.edu.vn>
2022-02-26 10:59:43 +0700

    Observer + Chain of Responsibility

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace ChainOfResponsibility
+{
+    public abstract class AbstractLogger
+    {
+        public static int INFO = 1;
+        public static int DEBUG = 2;
+        public static int ERROR = 3;
+
+        protected int level;
+
+        protected AbstractLogger nextLogger;
+
+        public void SetNextLogger(AbstractLogger nextLogger)
+        {
+            this.nextLogger = nextLogger;
+        }
+
+        public void LogMessage(int level, String message)
+        {
+            if (this.level <= level)
+            {
+                Write(message);
+            }
+            if (nextLogger != null)
+            {
+                nextLogger.LogMessage(level, message);
+            }
+        }
+
+        abstract protected void Write(String message);
+    }
+
+    public class ConsoleLogger : AbstractLogger
+    {
+        public ConsoleLogger(int level)
+        {
+            this.level = level;
+        }
+
+        override protected void Write(String message)
+        {
+            Console.WriteLine("Standard Console::Logger: " + message);
+        }
+    }
+
+    public class ErrorLogger : AbstractLogger
+    {
+        public ErrorLogger(int level)
+        {
+            this.level = level;
+        }
+
+        override protected void Write(String message)
+        {
+            Console.WriteLine("Error Console::Logger: " + message);
+        }
+    }
+
+    public class FileLogger : AbstractLogger
+    {
+
+        public FileLogger(int level)
+        {
+            this.level = level;
+        }
+
+        override protected void Write(String message)
+        {
+            Console.WriteLine("File::Logger: " + message);
+        }
+    }
+
+    class Program
+    {
+        private static AbstractLogger GetChainOfLoggers()
+        {
+            AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+            AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+            AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+            errorLogger.SetNextLogger(fileLogger);
+            fileLogger.SetNextLogger(consoleLogger);
+
+            return errorLogger;
+        }
+
+        static void Main(string[] args)
+        {
+            AbstractLogger loggerChain = GetChainOfLoggers();
+
+            loggerChain.LogMessage(AbstractLogger.INFO, "This is an information.");
+
+            Console.WriteLine("=======");
+
+            loggerChain.LogMessage(AbstractLogger.DEBUG, "This is an debug level information.");
+
+            Console.WriteLine("=======");
+
+            loggerChain.LogMessage(AbstractLogger.ERROR, "This is an error information.");
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer_Example
+{
+    public interface ISubject
+    {
+        public void RegisterObserver(IObserver observer);
+        public void RemoveObserver(IObserver observer);
+        public void NotifyObservers();
+    }
+
+    public class HotGirl : ISubject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+        public void NeedAttention()
+        {
+            Console.WriteLine("\nCough");
+            NotifyObservers();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (IObserver ob in observers)
+            {
+                ob.Update();
+            }
+        }
+    }
+
+    public interface IObserver
+    {
+        public void Update();
+    }
+
+    public class StupidGuy : IObserver
+    {
+        private string name;
+
+        public StupidGuy(string name)
+        {
+            this.name = name;
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("{0}: Are you OK?", name);
+        }
+    }
+
+    public class Neighbor : IObserver
+    {
+        public void Update()
+        {
+            Console.WriteLine("Call ambulance!!!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HotGirl hotGirl = new HotGirl();
+
+            IObserver simp = new StupidGuy("Ngoc Thinh");
+            IObserver simpLord = new StupidGuy("Chi Dat");
+            IObserver notMe = new StupidGuy("Tien Nam");
+
+            // Add to friend zone
+            hotGirl.RegisterObserver(simp);
+            hotGirl.RegisterObserver(simpLord);
+            hotGirl.RegisterObserver(notMe);
+
+            //hotGirl.NeedAttention();
+
+            //hotGirl.RemoveObserver(simp);
+
+            //hotGirl.NeedAttention();
+
+            IObserver unknown = new Neighbor();
+            hotGirl.RegisterObserver(unknown);
+            hotGirl.NeedAttention();
+
+        }
+    }
+}
